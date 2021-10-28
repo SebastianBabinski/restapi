6c6f6ffe2b5b6f7e4179ffe8a079821a900411df
SebastianBabinski <babinski.sebastian@gmail.com>
2021-10-28 11:51:30 +0200

    added cache feature

--- src/main/java/babinski/sebastian/restapi/service/PostService.java
+++ src/main/java/babinski/sebastian/restapi/service/PostService.java
@@ -5,6 +5,9 @@ import babinski.sebastian.restapi.model.Post;
 import babinski.sebastian.restapi.repository.CommentRepository;
 import babinski.sebastian.restapi.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -25,11 +28,13 @@ public class PostService {
         return postRepository.findAllPosts(PageRequest.of(page, PAGE_SIZE, Sort.by(sort, "id")));
     }
 
+    @Cacheable(cacheNames = "SinglePost", key = "#id")
     public Post getSinglePost(long id) {
         return postRepository.findById(id)
                 .orElseThrow();
     }
 
+    @Cacheable(cacheNames = "PostsWithComments")
     public List<Post> getPostsWithComments(int page, Sort.Direction sort) {
         List<Post> allPosts = postRepository.findAllPosts(PageRequest.of(page, PAGE_SIZE, Sort.by(sort, "id")));
         List<Long> ids = allPosts.stream()
@@ -53,6 +58,7 @@ public class PostService {
     }
 
     @Transactional
+    @CachePut(cacheNames = "SinglePost", key = "#result.id")
     public Post editPost(Post post) {
         Post postEdited = postRepository.findById(post.getId()).orElseThrow();
         postEdited.setTitle(post.getTitle());
@@ -61,6 +67,7 @@ public class PostService {
         return postEdited;
     }
 
+    @CacheEvict(cacheNames = "SinglePost")
     public void deletePost(long id) {
         postRepository.deleteById(id);
     }
